3b3a2db546563095e4caec8b351af2d6f46c9439
yaredow <yaredyilma11@gmail.com>
2025-10-12 19:21:12 +0300

    create more widgets for the create alarm screen

---
+++ lib/features/alarm/presentation/widgets/alarm_input_fields.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AlarmInputFields extends StatelessWidget {
+  const AlarmInputFields({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
