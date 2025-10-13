1a0362229063702f1ede6ddf330cfe2948965491
yaredow <yaredyilma11@gmail.com>
2025-10-13 13:28:22 +0300

    refactor

--- lib/features/alarm/presentation/widgets/day_selector_widget.dart
+++ lib/features/alarm/presentation/widgets/day_selector_widget.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:think_up/features/alarm/presentation/provider/alarm_provider.dart';
 
 class DaySelectorWidget extends StatefulWidget {
   const DaySelectorWidget({super.key});
@@ -18,59 +20,65 @@ class _SettingsScreenState extends State<DaySelectorWidget> {
     'Sat',
   ];
 
-  final Set<String> _selectedDays = {"Thu", "Fri"};
+  void _toggleDays(BuildContext context, day) {
+    final provider = Provider.of<AlarmProvider>(context, listen: false);
+    final Set<String> updatedDays = Set.from(provider.draftAlarm.days);
 
-  void _toggleDays(String day) {
-    setState(() {
-      if (_selectedDays.contains(day)) {
-        _selectedDays.remove(day);
-      } else {
-        _selectedDays.add(day);
-      }
-    });
+    if (updatedDays.contains(day)) {
+      updatedDays.remove(day);
+    } else {
+      updatedDays.add(day);
+    }
+
+    provider.updateDays(updatedDays);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          "Settings",
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.w500,
-            color: Colors.black54,
-          ),
-        ),
-        const SizedBox(height: 10),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: _daysOfWeek.map((day) {
-            final isSelected = _selectedDays.contains(day);
-            final activeColor = Colors.green.shade600;
-
-            return GestureDetector(
-              onTap: () => _toggleDays(day),
-              child: Container(
-                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
-                decoration: BoxDecoration(
-                  color: isSelected ? activeColor : Colors.transparent,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                child: Text(
-                  day,
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w500,
-                    color: isSelected ? Colors.white : Colors.black54,
-                  ),
-                ),
+    return Consumer<AlarmProvider>(
+      builder: (context, provider, child) {
+        final currentDays = provider.draftAlarm.days;
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Settings",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: Colors.black54,
               ),
-            );
-          }).toList(),
-        ),
-      ],
+            ),
+            const SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: _daysOfWeek.map((day) {
+                final isSelected = currentDays.contains(day);
+                final activeColor = Colors.green.shade600;
+
+                return GestureDetector(
+                  onTap: () => _toggleDays(context, day),
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                    decoration: BoxDecoration(
+                      color: isSelected ? activeColor : Colors.transparent,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Text(
+                      day,
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: isSelected ? Colors.white : Colors.black54,
+                      ),
+                    ),
+                  ),
+                );
+              }).toList(),
+            ),
+          ],
+        );
+      },
     );
   }
 }
